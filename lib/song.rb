9db06b7782b5adbcc18f8443a1f16856470ea209
Margot Konig <margot.konig@blackrock.com>
2019-05-24 02:23:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,8 +50,10 @@ class Song
     @song
   end
   def self.create_from_filename(filename)
-    self.new_from_filename(filename)
-    self.create
+    @song = self.new
+    @song.name = (filename.split(" - ")[1].chomp(".mp3"))
+    @song.artist_name = (filename.split(" - ")[0])
+    @@all << @song
     @song
   
   end
