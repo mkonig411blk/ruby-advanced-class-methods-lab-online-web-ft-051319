e60d137ed4a2bead48542d9f927b1a1ff9f5ef97
Margot Konig <margot.konig@blackrock.com>
2019-05-24 02:02:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,9 +33,11 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      @song
-    else self.create_by_name(name)
-    end
+      if self.find_by_name(name)
+       @song
+     else self.create_by_name(name)
+      end
 end
+
+def
 end
