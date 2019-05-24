b24a02eda1d91aea801ecd6674542bd598bf8c67
Margot Konig <margot.konig@blackrock.com>
2019-05-24 01:54:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,10 +17,13 @@ class Song
   end
 
   def self.new_by_name(name)
+    @song = self.new
+    @song.name = name
+    @song
+  end
+  
+  def self.create_by_name(name)
     @song = self.create
     @song.name = name
     @song
-    
-
-  end
 end
